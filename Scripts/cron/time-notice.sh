ebcd1e399e5fc8f42e5d2c3dc953c70cd9c6d0d3
Shivanshu <semwalshivanshu@gmail.com>
2024-01-24 20:15:33 +0530

    new config

--- Scripts/cron/time-notice.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export XDG_RUNTIME_DIR=/run/user/$(id -u)
-notify-send "Time: $(date +'%I:%M %p')" "$(fortune -a computers -a linux -s)" -i clock
-echo "$(date +'%I:%M %p')" | flite
--- Scripts/i3lock/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-$HOME/Scripts/i3lock/i3lock-simple/lock.sh
-# $HOME/Scripts/i3lock/lock.sh
